5ac7d5513a54dc44e97c0e1f84360bbe02f9b6ad
Julio <ljuliom@gmail.com>
2014-10-27 17:23:59 -0500

    rest api dogs

---
+++ app/controllers/api/dogs_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  class DogsController < ApplicationController
+
+    def index
+      @dogs = Dog.all
+      render json: @dogs, status: 200
+    end
+
+    def show
+      @dog = Dog.find(params[:id])
+      render json: @dog, status: 200
+    end
+
+    def create
+      @dog = Dog.new(dog_params)
+
+      if @dog.save
+        render json: @dog, status: 201, location: api_v1_dog_url(@dog[:id])
+      else
+        render json: @dog.errors, status: 422
+      end
+    end
+
+    private
+    
+      def dog_params
+        params.require(:dog).permit(:name, :breed, :sex, :size)
+      end
+  end
+end
