0d431a98ad3938817e76a97b5f1d4875e43d60c8
Julio <ljuliom@gmail.com>
2014-10-27 17:12:02 -0500

    generate model Dof

---
+++ db/migrate/20141027221137_create_dogs.rb
@@ -0,0 +1,12 @@
+class CreateDogs < ActiveRecord::Migration
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+      t.string :sex
+      t.string :size
+
+      t.timestamps
+    end
+  end
+end
